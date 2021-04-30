10eed412ad14c709d9b963d2d5925434d7c9165e
ԹՅɿՏɧɿkԵɧ ʝԾՏ <73195851+Hillard-har@users.noreply.github.com>
2021-04-30 17:36:39 +0530

    Create  config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+
+class Config(object):
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+      API_ID = int(os.environ.get("APP_ID", 12345))
+      API_HASH = os.environ.get("API_HASH")
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
