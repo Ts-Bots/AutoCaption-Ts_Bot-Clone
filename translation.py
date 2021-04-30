721eee54f6a249be6a0bb2d18bace8453df4936a
ԹՅɿՏɧɿkԵɧ ʝԾՏ <73195851+Hillard-har@users.noreply.github.com>
2021-04-30 17:39:02 +0530

    Create  translation.py

---
+++ translation.py
@@ -0,0 +1,100 @@
+class Translation(object):
+
+      
+      START_TEXT = """
+
+😃 Hai {},
+
+I am a simple auto caption bot
+I can automatically add pre-setted caption to the files in channels
+I am currently working for a channel if you want to use me, Use my source code and Deploy me
+
+⚜️ Maintained By 👉 @{}
+"""    
+
+      HELP_TEXT = """
+
+<u>💡 𝐇𝐞𝐥𝐩</u> 
+
+
+👉 Add me as a Admin in your channel with edit permission
+
+👉 Add your caption in heroku Config Var
+   
+<i>(Support Markdown)</i>
+
+👉 Forward your files in your channel that's it
+
+"""    
+              
+
+     
+      ABOUT_TEXT = """
+
+📕 𝐀𝐛𝐨𝐮𝐭 𝐌𝐞
+
+
+○ ᴍʏ ɴᴀᴍᴇ : [Auto Caption bot](https://t.me/joinchat/KFle6Zyi5H0zNzNl)
+
+○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ 
+
+○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ 
+
+○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ 
+
+○ ᴠᴇʀsɪᴏɴ : 1.0.0
+
+○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : [ᴛᴏᴜᴄʜ ᴍᴇ 🤗](https://github.com/Hillard-har/AutoCaption-Ts_Bot)
+
+○ ᴄʀᴇᴀᴛᴏʀ :  [ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ](https://t.me/trackstudio)
+ 
+__ᴘʟᴇᴀsᴇ ɴᴏᴛᴇ ɪ ᴀᴍ ɴᴏᴛ ᴀ ᴘʀᴏ ᴄᴏᴅᴇʀ ᴀɴᴅ ᴛʜɪs ɪs ᴍʏ ғɪʀsᴛ ᴛɪᴍᴇ ɪ ᴀᴍ ɢɪᴠɪɴɢ ᴀɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴏғ ʙᴏᴛ ғᴏʀ ᴇᴠᴇʀʏᴏɴᴇ ᴡʜɪᴄʜ ᴡᴀs ᴡʀɪᴛᴇʀ ɪɴ ᴍʏ ᴏᴡɴ ʜᴀɴᴅs, sᴏ ɪғ ᴀɴʏ ʙᴜɢs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ sᴀʏ
+ɪᴛ ᴍᴀʏ ʙᴇ ɴᴏᴛ ᴛʜᴇ ʙᴇsᴛ ʙᴜᴛ ғᴏʀ ᴍᴇ ɪᴛ ɪs ᴛʜᴇ ʙᴇsᴛ 🥰
+
+ᴛʜᴀɴᴋs ᴛᴏ ᴍʏ ʙᴇʟᴏᴠᴇᴅ ғʀɪᴇɴᴅ [ᴀɴᴏɴʏᴍᴏᴜs](https://t.me/Anoymous_Ns)__
+
+**[©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs](https://t.me/ts_bots)**
+
+"""
+
+      MARKDOWN_TEXT = """
+
+<u>𝐀𝐛𝐨𝐮𝐭 𝐌𝐚𝐫𝐤𝐝𝐨𝐰𝐧</u>
+
+
+👉 <b>Bold text</b>
+      
+📌 <code>**text**</code> 
+
+👉 <b>Italic text</b>
+
+📌 <code>__text__</code> 
+
+👉 <b>Code text</b>
+
+📌 <code>`text`</code>   
+
+👉 <b>Hyperlink text</b>
+
+📌 <code>[hyperlink_text](https://t.me/Ts_bots)</code> 
+
+<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
+
+"""
+
+# Bot status display
+
+      STATUS_DATA = """
+
+🔰 <u>𝐁𝐎𝐓 𝐒𝐓𝐀𝐓𝐔𝐒</u>
+
+🖌️ <b>Current Caption :</b>
+
+{}
+
+📐 <b>Current Position :</b> {}
+
+<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
+
+"""
+
