f7d7d1a09a932e0179f514056688bcbfe43c4154
Mʀ. A <73195851+Hillard-har@users.noreply.github.com>
2021-05-29 10:52:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,7 @@ class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-
-      # Better don't touch caption text in here 
-      CAPTION_TEXT = "" 
-
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "") 
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
       ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
