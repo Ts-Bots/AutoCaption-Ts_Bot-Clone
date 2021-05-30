08bb70541150593c2e05eca85ec73cdcdd6ccd22
Ts_Bots <81805185+Ts-Bots@users.noreply.github.com>
2021-05-30 12:05:56 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -203,8 +203,9 @@ async def button(bot, cmd: CallbackQuery):
           )
     elif "status_data" in cb_data:
           if Config.ADMIN_ID == int(cmd.message.chat.id):
+             caption_text = await get_caption(cmd.from_user.id)
              await cmd.message.edit(
-                  text=Translation.STATUS_DATA.format(Config.CAPTION_TEXT, Config.CAPTION_POSITION),
+                  text=Translation.STATUS_DATA.format(caption_text, Config.CAPTION_POSITION),
                   parse_mode="html", 
                   disable_web_page_preview=True, 
                   reply_markup=InlineKeyboardMarkup(
