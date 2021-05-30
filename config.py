59f3585fcebf11307207ee1a9cdb29f5f33a881e
Ts_Bots <81805185+Ts-Bots@users.noreply.github.com>
2021-05-30 12:52:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-      #CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "") 
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
       ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
