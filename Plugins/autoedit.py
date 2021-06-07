bad9ee4698460e4707e873f13e2016b190cf8644
Mʀ. A <73195851+Hillard-har@users.noreply.github.com>
2021-06-07 23:26:18 +0530

    Fixed caption "None" Error

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -27,8 +27,10 @@ async def editing(bot, message):
           if (message.document or message.video or message.audio):
              try:
                 file_caption = f"**{message.caption}**"
+                if file_caption == "None":
+                   file_caption = " "
              except:
-                file_caption = ""
+                file_caption = " "
                 pass 
       except:
           pass
